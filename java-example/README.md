b472033e6b071d45aae4a3304c3719461b910a18
Kwame Akuffo <Kwame.Akuffo@easyjet.com>
2017-09-10 19:59:40 +0100

    first commit

---
+++ java-example/README.md
@@ -0,0 +1,7 @@
+Updated to cucumber-jvm 1.2.2.
+
+To execute integration tests:
+
+```
+mvn integration-test
+```
---
+++ java-example/src/it/java/com/weblogism/cucumberjvm/javaexample/RunCukesIT.java
@@ -0,0 +1,11 @@
+package com.weblogism.cucumberjvm.javaexample;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "json:target/report.json")
+public class RunCukesIT {
+}
